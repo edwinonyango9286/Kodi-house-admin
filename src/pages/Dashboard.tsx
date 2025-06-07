1662edfc397020b913ee09b6893c5d45a59d74aa
edwin onyango <ookoedwin29@gmail.com>
2025-06-07 03:43:01 -0400

    feat|| admin dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography, Paper ,Avatar} from '@mui/material';
+import { Box, Typography, Paper ,Avatar, Divider, IconButton} from '@mui/material';
 import dotsVerticalIcon from "../assets/logos and Icons-20230907T172301Z-001/logos and Icons/dots vertical icon.svg"
 import arrowUpIcon from "../assets/logos and Icons-20230907T172301Z-001/logos and Icons/arrow up icon.svg"
 import chatIcon from "../assets/logos and Icons-20230907T172301Z-001/logos and Icons/chat Icon.svg"
@@ -7,16 +7,20 @@ import chartRedIcon from "../assets/logos and Icons-20230907T172301Z-001/logos a
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 import {Link } from "react-router-dom"
 import userImage from "../assets/Avatars square-20230907T172556Z-001/Avatars square/WebP/Demi Wilkinson.webp"
+import adilFloyd from "../assets/Avatars square-20230907T172556Z-001/Avatars square/WebP/Adil Floyd.webp"
+import { useState } from 'react';
+import dotVerticalIcon from "../assets/logos and Icons-20230907T172301Z-001/logos and Icons/dots vertical icon.svg"
 
 
 const Dashboard = () => {
-
   const data:[] =[]
+  const [selectedTimePeriod, setSelectedTimePeriod] = useState<string>("Weekly")
+  
   return (
     <Box sx={{width:"100%",}}>
       <Box sx={{width:"100%",display:"flex",alignItems:"start",gap:"12px", flexDirection:"column"}}>
       <Box sx={{ width:"100%", display: 'flex', gap:"24px", marginBottom:"24px", }}>
-        <Paper elevation={0} sx={{ display:"flex", flexDirection:"column", gap:"12px", border:"1px solid #EAECF0", borderRadius:"8px", cursor:"pointer", padding:"20px 16px", flex: 1 }}>
+        <Paper elevation={0} sx={{ display:"flex", flexDirection:"column", gap:"12px", border:"1px solid #EAECF0", borderRadius:"8px", cursor:"pointer", padding:"20px 16px", flex: 1,boxShadow: "0px 1px 2px 0px rgba(0, 0, 0, 0.06), 0px 1px 3px 0px rgba(0, 0, 0, 0.10)" }}>
           <Box sx={{display:"flex", justifyContent:"space-between"}}>
            <Typography sx={{color:"#1F2937", fontSize:"20px", fontWeight:"700"}} variant="h6">Total Landlords</Typography>
            <img src={dotsVerticalIcon} alt="dotsVerticalIcon"/>
@@ -34,7 +38,7 @@ const Dashboard = () => {
         </Paper>
 
 
-       <Paper elevation={0} sx={{ display:"flex", flexDirection:"column", gap:"12px", border:"1px solid #EAECF0", borderRadius:"8px", cursor:"pointer", padding:"20px 16px", flex: 1 }}>
+       <Paper elevation={0} sx={{ display:"flex", flexDirection:"column", gap:"12px", border:"1px solid #EAECF0", borderRadius:"8px", cursor:"pointer", padding:"20px 16px", flex: 1,boxShadow: "0px 1px 2px 0px rgba(0, 0, 0, 0.06), 0px 1px 3px 0px rgba(0, 0, 0, 0.10)" }}>
           <Box sx={{display:"flex", justifyContent:"space-between"}}>
            <Typography sx={{color:"#1F2937", fontSize:"20px", fontWeight:"700"}} variant="h6">Total Tenants</Typography>
            <img src={dotsVerticalIcon} alt="dotsVerticalIcon"/>
@@ -51,7 +55,7 @@ const Dashboard = () => {
           </Box>
         </Paper>
 
-        <Paper elevation={0} sx={{ display:"flex", flexDirection:"column", gap:"12px", border:"1px solid #EAECF0", borderRadius:"8px", cursor:"pointer", padding:"20px 16px", flex: 1 }}>
+        <Paper elevation={0} sx={{ display:"flex", flexDirection:"column", gap:"12px", border:"1px solid #EAECF0", borderRadius:"8px", cursor:"pointer", padding:"20px 16px", flex: 1,boxShadow: "0px 1px 2px 0px rgba(0, 0, 0, 0.06), 0px 1px 3px 0px rgba(0, 0, 0, 0.10)" }}>
           <Box sx={{display:"flex", justifyContent:"space-between"}}>
            <Typography sx={{color:"#1F2937", fontSize:"20px", fontWeight:"700"}} variant="h6">Total Properties</Typography>
            <img src={dotsVerticalIcon} alt="dotsVerticalIcon"/>
@@ -68,7 +72,7 @@ const Dashboard = () => {
           </Box>
         </Paper>
 
-        <Paper elevation={0} sx={{ display:"flex", flexDirection:"column", gap:"12px", border:"1px solid #EAECF0", borderRadius:"8px", cursor:"pointer", padding:"20px 16px", flex: 1 }}>
+        <Paper elevation={0} sx={{ display:"flex", flexDirection:"column", gap:"12px", border:"1px solid #EAECF0", borderRadius:"8px", cursor:"pointer", padding:"20px 16px", flex: 1, boxShadow: "0px 1px 2px 0px rgba(0, 0, 0, 0.06), 0px 1px 3px 0px rgba(0, 0, 0, 0.10)" }}>
           <Box sx={{display:"flex", justifyContent:"space-between"}}>
            <Typography sx={{color:"#1F2937", fontSize:"20px", fontWeight:"700"}} variant="h6">Total Units</Typography>
            <img src={dotsVerticalIcon} alt="dotsVerticalIcon"/>
@@ -87,9 +91,30 @@ const Dashboard = () => {
 
       </Box>
 
-      <Box sx={{ width:"100%", display:"flex",gap:"20px"}}> 
-        <Paper elevation={0} sx={{ width:"76%", height:"400px", backgroundColor:"#fff"}}>
-          <BarChart width={500} height={300} data={data}>
+      <Box sx={{ width:"100%", display:"flex",gap:"20px"}}>  
+        <Box sx={{ width:"76%", display:"flex", gap:"20px", flexDirection:"column"}}>
+
+         <Paper elevation={0} sx={{padding:"24px",width:"100%", height:"400px", backgroundColor:"#fff",boxShadow: "0px 1px 2px 0px rgba(0, 0, 0, 0.06), 0px 1px 3px 0px rgba(0, 0, 0, 0.10)"}}>
+          <Box sx={{ width:"100%",display:"flex",flexDirection:"column", gap:"10px"}}> 
+            <Box sx={{width:"100%", display:"flex", justifyContent:"space-between"}}>
+              <Typography variant='body2'  sx={{width:"50%",color:"#1F2937", fontWeight:"700", fontSize:"20px", textAlign:"start", }}>Total payment (Ksh 11,353,373.67)</Typography>
+              <Box sx={{display:"flex", justifyContent:"end", gap:"8px", width:"50%"}}>
+                <Box  onClick={()=>setSelectedTimePeriod("Weekly")} sx={{ cursor:"pointer", width:"72px", justifyContent:"center", display:"flex", flexDirection:"column", gap:"2px"}}>
+                  <Typography sx={{ alignSelf:"center", color: selectedTimePeriod === "Weekly" ? "#2563EB" :"#4B5563",  fontSize:"14px", fontWeight:"600"}}>Weekly</Typography>
+                 { selectedTimePeriod === "Weekly" && <Divider sx={{borderWidth:"2px", borderRadius:"4px", width:"px", backgroundColor:"#2563EB" }}/>}
+                </Box>
+                <Box onClick={()=>setSelectedTimePeriod("Monthly")} sx={{ cursor:"pointer", width:"72px", justifyContent:"center", display:"flex", flexDirection:"column", gap:"2px"}}>
+                  <Typography sx={{ alignSelf:"center", color: selectedTimePeriod === "Monthly" ? "#2563EB" :"#4B5563",  fontSize:"14px", fontWeight:"600"}}>Monthly</Typography>
+                  { selectedTimePeriod === "Monthly" && <Divider sx={{borderWidth:"2px", borderRadius:"4px", width:"px", backgroundColor:"#2563EB" }}/>}
+                </Box>
+                <IconButton sx={{ }}>
+                  <img src={dotVerticalIcon} alt="dotVerticalIcon" style={{ width:"25px", height:"25px"}} />
+                </IconButton>
+
+              </Box>
+
+            </Box>
+           <BarChart width={500} height={300} data={data}>
             <CartesianGrid strokeDasharray="3 3" />
             <XAxis dataKey="name" />
             <YAxis />
@@ -97,9 +122,41 @@ const Dashboard = () => {
             <Legend />
             <Bar dataKey="value" fill="#1976d2" />
           </BarChart>
-        </Paper>
+          </Box>
+         </Paper>
 
-        <Paper elevation={0} sx={{ padding:"20px 16px", width:"24%", height:"400px", backgroundColor:"#fff"}}>
+         <Box sx={{ display:"flex", gap:"20px", width:"100%",  }}>
+          <Paper elevation={0} sx={{ display:"flex" , flexDirection:"column", gap:"16px", padding:"24px", width:"50%",height:"400px", backgroundColor:"#fff", boxShadow: "0px 1px 2px 0px rgba(0, 0, 0, 0.06), 0px 1px 3px 0px rgba(0, 0, 0, 0.10)" }}>
+            <Typography sx={{ textAlign:"start", fontSize:"20px", fontWeight:"600", color:"#111827"}}>Latest Landlords</Typography>
+            <Box sx={{ width:"100%", display:"flex", justifyContent:"space-between"}}>
+              <Box sx={{ display:"flex", gap:"10px", width:"50%"}}>
+                <Avatar src={adilFloyd} alt=''  sx={{ }}/>
+                <Box sx={{ display:"flex", flexDirection:"column"}}>
+                  <Typography variant='body2' sx={{fontSize:"16px", fontWeight:"600", color:"#374151"}}>Neil Sims</Typography>
+                  <Typography sx={{ fontSize:"12px", fontWeight:"400", color:"#374151"}}>email@example.com</Typography>
+                </Box>
+              </Box>
+              <Box sx={{ width:"50%" }}>
+                <Typography variant='body2' sx={{ textAlign:"end", fontSize:"16px", fontWeight:"600"}}>Found Property Group</Typography>
+              </Box>
+            </Box>
+          </Paper> 
+           <Paper elevation={0} sx={{ display:"flex" , gap:"16px", flexDirection:"column", padding:"24px", width:"50%",height:"400px", backgroundColor:"#fff", boxShadow: "0px 1px 2px 0px rgba(0, 0, 0, 0.06), 0px 1px 3px 0px rgba(0, 0, 0, 0.10)" }}>
+            <Typography sx={{ textAlign:"start", fontSize:"20px", fontWeight:"600", color:"#111827"}}>Top properties</Typography>
+            <Box sx={{ width:"100%", display:"flex", justifyContent:"space-between"}}>
+              <Box sx={{ width:"50%" , display:"flex" , flexDirection:"column"}}>
+                <Typography sx={{fontSize:"16px", fontWeight:"600", color:"#111827"}}>Elgin St. Celina Places</Typography>
+                <Typography sx={{ fontWeight:"400", fontSize:"12px", color:"#3B82F6" }}>Michael Onkwani</Typography>
+              </Box>
+              <Box sx={{ width:"50%"}}>
+                <Typography sx={{ textAlign:"end",fontSize:"16px", fontWeight:"600",color:"#111827"}}>710 <span style={{fontSize:"16px", fontWeight:"400", color:"#4B5563"}}>Rents</span></Typography>
+              </Box>
+            </Box>
+          </Paper>
+         </Box>
+        </Box>
+
+        <Paper elevation={0} sx={{ padding:"20px 16px", width:"24%", height:"820px", backgroundColor:"#fff" , boxShadow: "0px 1px 2px 0px rgba(0, 0, 0, 0.06), 0px 1px 3px 0px rgba(0, 0, 0, 0.10)"}}>
           <Box sx={{display:"flex", flexDirection:"column", gap:"10px"}}>
            <Box sx={{ width:"100%", display:"flex", alignItems:"center", justifyContent:"space-between"}}>
               <Typography sx={{ fontSize:"20px", fontWeight:"700", color:"#101828"}}>Activity</Typography>
@@ -114,8 +171,15 @@ const Dashboard = () => {
            </Box>
           </Box>
         </Paper> 
-
       </Box>
+
+      <Paper elevation={0} sx={{padding:"20px", width:"100%",height:"400px",backgroundColor:"#fff",marginTop:"10px", boxShadow: "0px 1px 2px 0px rgba(0, 0, 0, 0.06), 0px 1px 3px 0px rgba(0, 0, 0, 0.10)"}}>
+        <Box sx={{ display:"flex", flexDirection:"column",gap:"4px"}}>
+          <Typography sx={{fontWeight:"600", fontSize:"20px", color:"#111827", textAlign:"start"}}>Transactions</Typography>
+          <Typography sx={{fontWeight:"400", fontSize:"14px", color:"##6B7280", textAlign:"start"}}>This is a list of latest transactions.</Typography>
+        </Box>
+      </Paper>
+      
 
       </Box>
     </Box>
