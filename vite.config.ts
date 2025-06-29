35dc7a7548095ca63f99bdd047e554b9272d797f
edwin onyango <ookoedwin29@gmail.com>
2025-06-29 08:26:10 -0400

    fix|| build for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,22 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), 'VITE_');
-
+  const env = loadEnv(mode, process.cwd(), "");
   return {
+    base:"/",
     plugins: [react()],
     server: {
       port: 6064,
       open: true,
-      host:true,
+      host: true,
+    },
+    build: {
+      outDir: "dist",
+      emptyOutDir: true,
     },
     define: {
-      __APP_ENV__: JSON.stringify(env.APP_ENV)
+      "process.env": env,
     },
-  }
-})
+  };
+});
