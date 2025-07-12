17caad8c3677592f96385e8e1768a46072ca400a
edwin onyango <ookoedwin29@gmail.com>
2025-07-12 05:32:51 -0400

    feat|| add /api/v1 for dev deployment

--- src/utils/newRequest.ts
+++ src/utils/newRequest.ts
@@ -1,24 +1,23 @@
 import axios from "axios";
 import type { AxiosInstance, AxiosRequestConfig } from "axios";
 
-const base_url: string = import.meta.env.VITE_BASE_URL ?? '';
+const base_url: string = import.meta.env.VITE_BASE_URL ?? "";
+const finalUrl: string = `${base_url}/api/v1`;
 
 interface CustomAxiosConfig extends AxiosRequestConfig {
   withCredentials: true;
-  timeout: number;
   headers: {
-    'Accept': 'application/json';
-    'Content-Type': 'application/json';
+    Accept: "application/json";
+    "Content-Type": "application/json";
     [key: string]: string;
   };
 }
 
 export const newRequest: AxiosInstance = axios.create({
-  baseURL: base_url,
-  timeout: 10000,
+  baseURL: finalUrl,
   headers: {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json'
+    Accept: "application/json",
+    "Content-Type": "application/json",
   },
-  withCredentials: true
-} as CustomAxiosConfig); 
+  withCredentials: true,
+} as CustomAxiosConfig);
