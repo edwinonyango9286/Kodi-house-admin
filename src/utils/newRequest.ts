1c3a998bfd634947921047fee6f8bf2ee41293a9
edwin onyango <ookoedwin29@gmail.com>
2025-07-12 05:43:42 -0400

    feat|| update api request

--- src/utils/newRequest.ts
+++ src/utils/newRequest.ts
@@ -2,7 +2,6 @@ import axios from "axios";
 import type { AxiosInstance, AxiosRequestConfig } from "axios";
 
 const base_url: string = import.meta.env.VITE_BASE_URL ?? "";
-const finalUrl: string = `${base_url}/api/v1`;
 
 interface CustomAxiosConfig extends AxiosRequestConfig {
   withCredentials: true;
@@ -14,7 +13,7 @@ interface CustomAxiosConfig extends AxiosRequestConfig {
 }
 
 export const newRequest: AxiosInstance = axios.create({
-  baseURL: finalUrl,
+  baseURL: base_url,
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
